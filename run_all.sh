b96cf91888b2310dbb3c5691d6655864f4bc8774
Abeoseh Flemister <aflemis1@str-i3.charlotte.edu>
2025-01-08 11:11:10 -0500

    updated .sh and .sbatch files

--- run_all.sh
+++ run_all.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-if [ -f ./csv_files/AUCs/associated_2192/builtenv_AUCs.csv ]; then
-	rm ./csv_files/AUCs/associated_2192/builtenv_AUCs.csv
-fi
+## to run this file: 
+# run_all.sh input_folder_name output_folder_name amount_of_studies
+# run_all.sh associated associated 4
 
-if [ -f ./csv_files/AUCs/associated_2192/builtenv_AUC_pvals.csv ]; then
-	rm ./csv_files/AUCs/associated_2192/builtenv_AUC_pvals.csv
-fi
+# If the folder already exists allow user to decide to overwrite
+while [ -d ./output/$2 ]; do
+	read -p "This folder exists. Overwrite? Y (yes) or N (no) " yn
+		case $yn in
+			[Yy]* ) rm -r ./output/$1; break;;
+			[Nn]* ) exit;;
+			* ) echo "Please answer Y (yes) or N (no)";;
+		esac
+done
 
-mkdir ./output/$1
-mkdir ./output/$1/AUCs
-mkdir ./output/$1/DEBIAS-M_runs
-mkdir ./output/$1/ROC_histograms
 
-search_dir=./csv_files/$1/DEBIAS-M_runs
-amount=$2
+mkdir ./output/$2
+mkdir ./output/$2/logs
+mkdir ./output/$2/AUCs
+mkdir ./output/$2/DEBIAS-M_runs
+mkdir ./output/$2/ROC_histograms
 
-rm "$search_dir/"*
+
+search_dir=./output/$2/DEBIAS-M_runs
+amount=$3
+
 
 for i in $( eval echo {1..$amount} )
 do
-	sbatch run_all_pt1.sbatch $i 100 $1
-	sbatch run_all_pt2.sbatch $i
+	sbatch run_all_pt1.sbatch $i 100 $1 $2
+	sbatch run_all_pt2.sbatch $i $1 $2
 	echo $i "of $amount done"
 done
 
@@ -30,24 +38,32 @@ done
 count_files() {
 	file_count=$(find "$search_dir/"*debiased* | wc -l)
 	echo $file_count
-
 }
 
 while [ $(count_files) -lt $amount ]; do
 	echo "Waiting for $amount files"
 	echo $file_count
-	sleep 300 # waiting 5 minute before checking again
+	sleep 150 # waiting 2 minute 30s before checking again
 done
 
 echo "$amount files have been detected"
 
 
-
+# entry is each DEBIASed file
 for entry in "$search_dir/"*debiased*
 do
-	sbatch run_all_pt3.sbatch $entry 100 $(echo $entry | cut -d"_" -f7- | cut -d"." -f1) $1
-	echo $(echo $entry | cut -d"_" -f7- | cut -d"." -f1)
-	echo $entry
+	echo "$entry"
+	# extract study IDs
+	Study_ID="${entry##*/}"  # remove everything before the last /
+	Study_ID="${Study_ID##*_}"  # Remove everything before the last _
+	Study_ID="${Study_ID%.*}"    # Remove everything after the last .
+
+	# Untested ALT
+	# echo "$file" | grep -oP '([^_]+)(?=\.\w+$)' 
+
+	sbatch run_all_pt3.sbatch $entry 100 $Study_ID $1
+
+	echo "$Study_ID"
 done
 
 echo "done with all"
--- test.sh
+++ test.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-#search_dir=./csv_files/DEBIAS-M_runs/associated_2192
+search_dir=./csv_files/DEBIAS-M_runs/associated_2192
 
 
 
-#for entry in "$search_dir/"*debiased*
-#do
-#	sbatch run_all_pt3.sbatch $entry 100 $(echo $entry | cut -d"_" -f7- | cut -d"." -f1) 
-#	echo $(echo $entry | cut -d"_" -f7- | cut -d"." -f1)
-#	echo $entry
-#done
+for entry in "$search_dir/"*debiased*
+do
+	#echo $entry
+	entry="${entry##*/}"  # remove everything before the last /
+	entry="${entry##*_}"  # Remove everything before the last _
+	entry="${entry%.*}"    # Remove everything after the last .
+	echo "$entry"
+done
 
-mkdir ./output/$1
-mkdir ./output/$1/AUCs
-mkdir ./output/$1/DEBIAS-M_runs
-mkdir ./output/$1/ROC_histograms
+#mkdir ./output/$1
+#mkdir ./output/$1/AUCs
+#mkdir ./output/$1/DEBIAS-M_runs
+#mkdir ./output/$1/ROC_histograms
 
 echo "done with all"
 
